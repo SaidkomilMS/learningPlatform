9dfdfcf35db01d276dfb6e551f101cfe8274834d
Saidkomil Maxamadxujaev <9-saidkomil.m@users.noreply.gitlab.cloudgate.uz>
2024-08-09 11:29:18 +0500

    db connection from env variables

--- config/config.go
+++ config/config.go
@@ -1,5 +1,10 @@
 package config
 
+import (
+	"fmt"
+	"os"
+)
+
 type Config struct {
 	DBSource     string
 	JWTLifeTime  uint
@@ -10,6 +15,7 @@ type Config struct {
 func LoadConfig() *Config {
 	DBString := fmt.Sprintf("user=%s password=%s dbname=%s host=%s sslmode=disable",
 		os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), os.Getenv("DB_HOST"))
+	fmt.Println("DBConnection string:", DBString)
 	return &Config{
 		//DBSource:     "host=" + host + "user=" + user + " password=" + password + " dbname=" + dbname + " port=" + port + " sslmode=disable TimeZone=Asia/Tashkent",
 		DBSource:     DBString,
